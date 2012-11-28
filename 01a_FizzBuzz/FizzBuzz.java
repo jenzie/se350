92e3da4d50589e46acb1b58a768116890fca276e
Jenny Zhen <jxz6853@rit.edu>
2012-11-28 13:29:51 -0500

    FizzBuzz works now.

--- 01a_FizzBuzz/FizzBuzz.java
+++ 01a_FizzBuzz/FizzBuzz.java
@@ -4,7 +4,7 @@
  *
  */
 public class FizzBuzz {
-	public void fizz_buzz() {
+	public static void main(String[] args) {
 		for(int i = 0; i <= 100; i++) {
 			if(i % 3 == 0 && i % 5 == 0)
 				System.out.println("FizzBuzz");
