4142366150cb4bc74679927dc923be4a1ef7a985
Jenny Zhen <jxz6853@rit.edu>
2012-12-03 11:51:07 -0500

    Need to debug...

--- 02a_Histogram/histogram.c
+++ 02a_Histogram/histogram.c
@@ -7,6 +7,7 @@
 
 #define NLETTERS (26)	/* size of array, one entry per letter */
 #define MAXSTARS (70)	/* maximum number of stars in a histogram line */
+#define A_VALUE  (65)   /* use to determine distance of char from capital 'A' */
 
 int count[NLETTERS] ;	/* counts for each of the 26 letters */
 
@@ -17,9 +18,39 @@ int count[NLETTERS] ;	/* counts for each of the 26 letters */
 extern void print_stars( int ns ) ;
 
 int main() {
+	int nchar ; /* next character read */
+	int count = 0; /* count for current letter */
+	int max_count = 0; /* highest frequency of letter */
+	int i, j, k, m;
 
-	/* REPLACE WITH THE CODE FOR HISTOGRAM */
+	/* assign getchar() to nchar, loop until end of file */
+	while((nchar = getchar()) != EOF) {
+		if(isalpha(nchar)) {
+			toupper(nchar);
+			count[nchar - A_VALUE] += 1;
+		}
+	}
+	
+	/* getting the highest count from all letters */
+	for(i = 0; i < NLETTERS; i++) {
+		if(count[i] > max_count)
+			max_count = count[i];
+	}
 
+	/* scaling the count */
+	int j;
+	for(j = 0; j < NLETTERS; j++) {
+		count = count[j];
+		count[j] = (MAXSTARS * count) / count;
+	}
+
+	/* print the stars for the histogram */
+	for(k = 0; k < NLETTERS; k++) {
+		/* print out the letter value */
+		printf("%c", (A_VALUE + k));
+		print_stars( count[k] );
+		printf("\n");
+	}
 	return 0 ;
 }
 
@@ -28,8 +59,11 @@ int main() {
  */
 
 void print_stars( int ns ) {
+	int m;
 
-	/* CODE TO PRINT OUT ASTERISKS */
+	/* print out the asterisks */
+	for(m = 0; m < ns; m++)
+		printf("%c", 42);
 
 	return ;
 }
