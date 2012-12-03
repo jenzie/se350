b036d40d1d9bd18e8165b87a5a408660e6ce00d8
Jenny Zhen <jxz6853@rit.edu>
2012-12-03 10:48:29 -0500

    Base file.

---
+++ 02a_Histogram/histogram.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#define FALSE (0)
+#define TRUE  (1)
+
+#define NLETTERS (26)	/* size of array, one entry per letter */
+#define MAXSTARS (70)	/* maximum number of stars in a histogram line */
+
+int count[NLETTERS] ;	/* counts for each of the 26 letters */
+
+/*
+ * The declaration for the function to print a sequence of asterisks.
+ */
+
+extern void print_stars( int ns ) ;
+
+int main() {
+
+	/* REPLACE WITH THE CODE FOR HISTOGRAM */
+
+	return 0 ;
+}
+
+/*
+ * Print out 'ns' stars (asterisks)
+ */
+
+void print_stars( int ns ) {
+
+	/* CODE TO PRINT OUT ASTERISKS */
+
+	return ;
+}
