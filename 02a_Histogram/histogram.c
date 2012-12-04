f9cc68c13904382fa106b90e8458a11b3193f610
Jenny Zhen <jxz6853@rit.edu>
2012-12-04 01:13:36 -0500

    Histogram should print correct number of asterisks.

--- 02a_Histogram/histogram.c
+++ 02a_Histogram/histogram.c
@@ -1,3 +1,14 @@
+/**
+author: Jenny Zhen
+date: 12.03.12
+language: C
+assignment: Histogram
+	Displays a scaled histogram of asterisks reqpresenting the frequency 
+	of letters in a text file.
+	gcc -o histogram histogram.c ./histogram < TomSawyer.txt
+	http://www.se.rit.edu/~se350/Class_Activities/02_Arrays+Strings/Histogram/Histogram.html
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
@@ -19,18 +30,19 @@ extern void print_stars( int ns ) ;
 
 int main() {
 	int nchar ; /* next character read */
-	int count = 0; /* count for current letter */
+	int curr_count = 0; /* count for current letter */
 	int max_count = 0; /* highest frequency of letter */
-	int i, j, k, m;
+	int i, j, k;
+
 
 	/* assign getchar() to nchar, loop until end of file */
 	while((nchar = getchar()) != EOF) {
 		if(isalpha(nchar)) {
-			toupper(nchar);
+			nchar = toupper(nchar);
 			count[nchar - A_VALUE] += 1;
 		}
 	}
-	
+
 	/* getting the highest count from all letters */
 	for(i = 0; i < NLETTERS; i++) {
 		if(count[i] > max_count)
@@ -38,16 +50,16 @@ int main() {
 	}
 
 	/* scaling the count */
-	int j;
 	for(j = 0; j < NLETTERS; j++) {
-		count = count[j];
-		count[j] = (MAXSTARS * count) / count;
+		curr_count = count[j];
+		if(curr_count > 0)
+			count[j] = (MAXSTARS * curr_count) / max_count;
 	}
 
 	/* print the stars for the histogram */
 	for(k = 0; k < NLETTERS; k++) {
 		/* print out the letter value */
-		printf("%c", (A_VALUE + k));
+		printf("%c ", (A_VALUE + k));
 		print_stars( count[k] );
 		printf("\n");
 	}
