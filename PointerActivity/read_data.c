11af8b527a579c8dbf26af3b31563152bcc45ddf
Jenny Zhen <jxz6853@rit.edu>
2012-12-10 11:14:48 -0500

    Skeleton files and makefile for c pointer activity.

---
+++ PointerActivity/read_data.c
@@ -0,0 +1,21 @@
+/*
+ * Implementation of the read_data module.
+ *
+ * See read_data.h for a description of the read_data function's
+ * specification.
+ *
+ * Note that the parameter declarations in this module must be
+ * compatible with those in the read_data.h header file.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "read_data.h"
+
+void read_data( /* you fill in the parameter declarations */ ) {
+	/* your declarations and statements REPLACE this comment */
+
+	return ;
+}
