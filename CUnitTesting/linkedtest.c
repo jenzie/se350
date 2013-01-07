55ef3df9ba9e6722de63ec3a91ee0028d4489836
Jenny Zhen <jxz6853@rit.edu>
2013-01-07 10:42:21 -0500

    Skeleton files for C unit testing activity.

---
+++ CUnitTesting/linkedtest.c
@@ -0,0 +1,39 @@
+/*
+ * linkedtest - unit test cases for linked.c using simplectest
+ * Tom Reichlmayr, RIT, 12/18/2006
+ */
+/************************************************************
+ main - test functions in this package
+ ************************************************************/
+/*
+*/
+
+#include "tests.h"
+#include "linked.h"
+
+// Start the overall test suite, "main()" starts here
+START_TESTS()  
+// these variables are visible to all test case macros
+     struct node* testlist = NULL;
+     int val;
+
+// "helper" functions could also be created as necessary, i.e. like JUnit setup()
+
+START_TEST("list length of one")
+     Push( &testlist, 1);
+     ASSERT_EQUALS( 1, Length( testlist ));
+END_TEST()
+
+START_TEST("add a second node and make this test fail")
+     Push( &testlist, 2);
+     ASSERT_EQUALS( 1, Length( testlist));
+END_TEST()
+
+START_TEST("pop top element of list")
+	   val = Pop( &testlist );
+	   ASSERT_EQUALS( 2, val );
+END_TEST()
+
+
+// End the overall test suite
+END_TESTS()   
---
+++ CUnitTesting/tests.h
@@ -0,0 +1,7 @@
+/**
+author: Jenny Zhen
+date: 01.07.13
+language: C
+assignment: C Unit Testing
+	http://www.se.rit.edu/~se350/Class_Activities/07_CUnitTesting/CTestIntro.htm
+*/
