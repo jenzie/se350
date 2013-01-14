bea5cd90319c6c70c1ed4e41b79331fcc61b7f49
Jenny Zhen <jxz6853@rit.edu>
2013-01-14 10:53:04 -0500

    Skeleton files for ruby histogram assignment.

---
+++ RubyHistogram/histogram.rb
@@ -0,0 +1,10 @@
+-begin
+author: Jenny Zhen
+date: 01.14.13
+language: Ruby
+file: histogram.rb
+assignment: Intro to Ruby
+	http://www.se.rit.edu/~se350/Class_Activities/11-RubyIntro/index.html
+-end
+
+
