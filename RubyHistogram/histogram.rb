ba076406cd3bf8501cc7b0359c01da44fd7b1db4
Jenny Zhen <jxz6853@rit.edu>
2013-01-14 11:52:22 -0500

    Need to fix bugs...

--- RubyHistogram/histogram.rb
+++ RubyHistogram/histogram.rb
@@ -1,10 +1,21 @@
--begin
-author: Jenny Zhen
-date: 01.14.13
-language: Ruby
-file: histogram.rb
-assignment: Intro to Ruby
-	http://www.se.rit.edu/~se350/Class_Activities/11-RubyIntro/index.html
--end
+##
+# author: Jenny Zhen
+# date: 01.14.13
+# language: Ruby
+# file: histogram.rb
+# assignment: Intro to Ruby
+#	http://www.se.rit.edu/~se350/Class_Activities/11-RubyIntro/index.html
+##
 
-
+bag = Hash.new(0)
+$stdin.each.do |line|
+	puts line
+	line.chomp!
+	line.downcase!
+	line.gsub!(/[^a-zA-Z ]/,"")
+	line.sub!(/^ +/,"")
+	line.split(/ +/).each do |word|
+		bag[word] += 1
+	end
+end
+puts bag
