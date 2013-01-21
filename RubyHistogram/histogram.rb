8ca901ae685d334cbf3e6dbf64aabf093aa0d51d
Jenny Zhen <jxz6853@rit.edu>
2013-01-21 06:08:43 -0500

    Completed ruby histogram.

--- RubyHistogram/histogram.rb
+++ RubyHistogram/histogram.rb
@@ -7,15 +7,24 @@
 #	http://www.se.rit.edu/~se350/Class_Activities/11-RubyIntro/index.html
 ##
 
-bag = Hash.new(0)
-$stdin.each.do |line|
-	puts line
-	line.chomp!
-	line.downcase!
-	line.gsub!(/[^a-zA-Z ]/,"")
-	line.sub!(/^ +/,"")
-	line.split(/ +/).each do |word|
-		bag[word] += 1
-	end
+if ARGV[0] != nil
+  minimum = ARGV[0].to_i
+else
+  minimum = 2
 end
-puts bag
+
+bag = Hash.new(0)
+$stdin.each do |line|
+  line.chomp!
+  line.downcase!
+  line.gsub!(/[^a-zA-Z ]/,"")
+  line.sub!(/^ +/,"")
+  line.split(/ +/).each do |word|
+	  bag[word] += 1
+  end
+end
+
+bag.select! { |key, value| value >= minimum }
+sorted = bag.sort_by { |x, y| [ -Integer(y), x ] }
+maximum = sorted.inject(0) { |max, el| max = [max, el[0].length].max }
+sorted.each { |key| printf "%-#{maximum}s %s\n", key[0], "*" * key[1] }
