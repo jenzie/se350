32dbada90787b8e871f17b457efd3f6697b5a637
Jenny Zhen <jxz6853@rit.edu>
2013-01-23 10:52:43 -0500

    Skeleton files for diet manager.

---
+++ DietManager/BasicFood.rb
@@ -0,0 +1,9 @@
+##
+# author: Jenny Zhen
+# date: 02.03.13
+# language: Ruby
+# file: BasicFood.rb
+# assignment: Diet Manager (Project 03)
+#   http://www.se.rit.edu/~se350/Projects/Project3/index.html
+##
+
