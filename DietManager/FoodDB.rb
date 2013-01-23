fc49d82258d04c5b814aed30b038978f98d9dc80
Jenny Zhen <jxz6853@rit.edu>
2013-01-23 14:31:18 -0500

    FoodDB.rb: Added functions to initialize, parse, and create food entries to be placed in database.

--- DietManager/FoodDB.rb
+++ DietManager/FoodDB.rb
@@ -7,3 +7,30 @@
 #   http://www.se.rit.edu/~se350/Projects/Project3/index.html
 ##
 
+require './Recipe.rb'
+require './BasicFood.rb'
+
+class FoodDB
+  def initialize?( file )
+	@database = Hash.new
+	
+	if File.file?( file )
+	  File.open( file , "r" ).each do |line|
+		item = line.split(",")
+		database[ item[0] ] = createItem( item )
+	  File.close
+	end
+	else
+	  puts "Usage: " ARGV[1] " database.txt log.txt"
+	end
+  end
+  
+  def createItem( item )
+	if item[1].eql?("b")
+	  return BasicFood.new( item[0], item[2] )
+	end
+	else if item[1].eql?("r")
+	  return Recipe.new( item )
+	end
+  end
+end # end class
