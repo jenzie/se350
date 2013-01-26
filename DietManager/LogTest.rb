da60ec5d3b1f5e06274f3263724c649b448fa4e3
Jenny Zhen <jxz6853@rit.edu>
2013-01-26 18:54:05 -0500

    LogTest.rb passes all unit tests for Log.rb. TO-DO: Add functionality for showAll

--- DietManager/LogTest.rb
+++ DietManager/LogTest.rb
@@ -12,4 +12,40 @@ require 'test/unit'
 
 class LogTest < Test::Unit::TestCase
 
+  def setup
+    @db = FoodDB.new( "FoodDB.txt" )
+	@log = Log.new( "DietLog.txt", @db )
+	@entry1 = [ "2013-02-01", "item_does_not_exist" ]
+	@entry2 = [ "2013-02-02", "Bread Slice" ]
+	@entry3 = [ "2013-02-02", "Jelly" ]
+  end
+  
+  def test_for_createEntry
+    assert( !@log.createEntry( @entry1 ),
+	  "Entry was created, but '#{@entry1[1]}' does not exist in database." )
+	assert( @log.createEntry( @entry2 ),
+	  "Entry2 was not created and added into the log." )
+	assert( @log.createEntry( @entry3 ),
+	  "Entry3 was not modified into the log." )
+  end
+  
+  def test_for_logForToday
+    assert( @log.logForToday( "Candy" ),
+	  "Entry 'Candy' was not added for today." )
+	assert( !@log.logForToday( "something" ),
+	  "Entry 'something' was added to log, but it does not exist." )
+  end
+  
+  def test_for_logForDate
+    assert( !@log.logForDate( "Gum", "12345678" ),
+	  "Entry1 was added to log, but date is invalid." )
+	assert( !@log.logForDate( "idk", "2013-02-01" ),
+	  "Entry2 was added to log, but item does not exist in database." )
+	assert( @log.logForDate( "Fried Egg", "2013-02-01" ),
+	  "Entry3 was not added to database." )
+  end
+  
+  def test_for_showAll
+  end
+
 end #end class
