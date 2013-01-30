ec6cae527432343da256945b1303db336ccb3769
Jenny Zhen <jxz6853@rit.edu>
2013-01-30 10:43:06 -0500

    to_phonetic() passes all unit tests.

---
+++ practicum-3/main.rb
@@ -0,0 +1,18 @@
+##
+# author: Jenny Zhen
+# date: 01.30.13
+# language: Ruby
+# file: main.rb
+# assignment: NATO Alphabet (Practicum 03)
+#   https://mycourses.rit.edu/d2l/lms/content/viewer/view.d2l?tId=2342072&ou=422264
+##
+
+require './phonetic'
+
+p = Phonetic.new
+input = STDIN.read
+input.each_line{|line|
+
+     ### YOUR CODE HERE ###
+     
+}
---
+++ practicum-3/test_phonetic.rb
@@ -0,0 +1,46 @@
+##
+# author: Jenny Zhen
+# date: 01.30.13
+# language: Ruby
+# file: test_phonetic.rb
+# assignment: NATO Alphabet (Practicum 03)
+#   https://mycourses.rit.edu/d2l/lms/content/viewer/view.d2l?tId=2342072&ou=422264
+##
+
+require './phonetic'
+require 'test/unit'
+
+class TestPhonetic < Test::Unit::TestCase
+	def setup
+	     @phonetic = Phonetic.new
+	end
+	
+	def test_01_to_phonetic_basic
+	     assert_equal("ROMEO UNIFORM BRAVO YANKEE", @phonetic.to_phonetic("ruby"))
+	end
+
+	def test_02_to_phonetic_with_capitals
+	     assert_equal("ROMEO INDIA TANGO", @phonetic.to_phonetic("RIT"),"Capitals not handled properly")
+	end
+
+	def test_03_to_phonetic_with_capitals_and_punct
+	     assert_equal("ROMEO INDIA TANGO", @phonetic.to_phonetic("RIT!"),"Capitals and punctuation not handled properly")
+	end
+
+	def test_04_from_phonetic
+	     assert_equal("ENGINEERING", @phonetic.from_phonetic("ECHO NOVEMBER GOLF INDIA NOVEMBER ECHO ECHO ROMEO INDIA NOVEMBER GOLF"))
+	end
+
+	def test_05_auto_from
+	     assert_equal("RIT", @phonetic.auto_detect("ROMEO INDIA TANGO"),"Auto-detect wasn't 'from'")
+	end
+
+	def test_06_auto_to
+	     assert_equal("ROMEO INDIA TANGO", @phonetic.auto_detect("RIT"),"Auto-detect wasn't 'to'")
+	end
+
+	def test_07_auto_should_be_from
+	     assert_equal("INDIA NOVEMBER DELTA INDIA ALPHA NOVEMBER ALPHA", @phonetic.auto_detect("INDIANA"))
+	end
+end
+
