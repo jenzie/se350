902d9e4be69532bf32f4ed32b336339e0aab0e35
Jenny Zhen <jxz6853@rit.edu>
2013-02-06 11:36:19 -0500

    Skeleton files for Rails songs activity.

---
+++ songs/songs/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Songs::Application.config.secret_token = 'dacf455bd0465c36e37b1822869205b88777199bc58a080c2a48d8cf86200573a22fcfb4644e634666c382adbdd09392a25e2fb7e1856f13add0fa71b8045866'
