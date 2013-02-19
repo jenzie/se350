92ec4bb0478346f1e8e40ff3c2b5d82898174c84
Jenny Zhen <jxz6853@rit.edu>
2013-02-19 15:20:12 -0500

    Added unit tests and fixed some bugs in the Movie name and rating.

--- practicum-4/MovieManager.rb
+++ practicum-4/MovieManager.rb
@@ -20,8 +20,8 @@ class MovieList
     @numberOfMovies
    end
   
-  def add(movieToAdd )
-    @movies.store( movieToAdd.name, movieToAdd)
+  def add(movieToAdd)
+    @movies.store(movieToAdd.name, movieToAdd)
     @numberOfMovies += 1  
   end
   
@@ -47,25 +47,29 @@ class MovieList
 #                     rating (1-10) and text review.
 #
 # Change Log - Identify the modifications made to the orginal code
-# (1) -
-# (2) -
-#  .
+# (1) - line 56 - added attr_reader to access the name of the movie
+# (2) - line 61 - changed to default the rating to 5, if none provided
+# (3) - lines 70-71 - added default value to rating if input out of bounds
+# (4) - line 68 - changed logic error when checking bounds to be use 'AND'
 # (n) -  
 
 class Movie
+	attr_reader :name
     attr_reader :rating
     attr_reader :review
     attr_writer :review
       
-    def initialize( aName, aRating, aReview="No Review Entered" )
-        @name=aName.upcase! 
-	self.rating=aRating
-	@review=aReview
+    def initialize( aName, aRating=5, aReview="No Review Entered" )
+        @name=aName.upcase
+		self.rating=aRating
+		@review=aReview
     end
 
     def rating=(value)
-	if value <=10 || value >=1 
-		@rating = value
-	end
-     end
+		if value <=10 && value >=1 
+			@rating = value
+		else
+			@rating = 5
+		end
+    end
 end
--- practicum-4/TestMovieManager.rb
+++ practicum-4/TestMovieManager.rb
@@ -8,4 +8,35 @@ class MovieManagerTest < Test::Unit::TestCase
     assert_not_nil( testList, "Should be able to create a MovieList collection")
   end
   
-end 
+  def test_movie_name
+	# test for all lowercase name
+	movie01 = Movie.new( "movie01", 1 )
+	assert_equal "MOVIE01", movie01.name
+	
+	# test for all uppercase name
+	movie02 = Movie.new( "MOVIE02", 2 )
+	assert_equal "MOVIE02", movie02.name
+	
+	# test for mixed case name
+	movie03 = Movie.new( "mOvIe03", 3 )
+	assert_equal "MOVIE03", movie03.name
+  end
+  
+  def test_movie_rating
+	# test for valid ratings
+	movie01 = Movie.new( "movie01", 1 )
+	movie02 = Movie.new( "MOVIE02", 10 )
+	assert_equal 1, movie01.rating
+	assert_equal 10, movie02.rating
+	
+	# test for no rating provided
+	movie03 = Movie.new( "movie03" )
+	assert_equal 5, movie03.rating
+	
+	# test for invalid ratings
+	movie04 = Movie.new( "movie04", 0 )
+	movie05 = Movie.new( "movie05", 11 )
+	assert_equal 5, movie04.rating
+	assert_equal 5, movie05.rating
+  end
+end
